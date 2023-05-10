c38097d445f9ae290fb0099b64abea49c5af8763
Matias Gomez <matias_gomez05@hotmail.com>
2023-05-10 20:06:22 -0300

    Commit inicial

---
+++ src/main/java/ar/edu/davinci/dvds20231cg0/DvDs20231cG0Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20231cg0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20231cG0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20231cG0Application.class, args);
+	}
+
+}
