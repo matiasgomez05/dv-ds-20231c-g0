85dd43408436375e3f1c7eee0ed8da6e66b346a1
Juan Manuel Saavedra <juaanms@gmail.com>
2023-06-25 13:13:24 -0300

    .

---
+++ src/main/java/ar/edu/davinci/dvds20231cg9/service/PrendaService.java
@@ -0,0 +1,22 @@
+package ar.edu.davinci.dvds20231cg9.service;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ar.edu.davinci.dvds20231cg9.domain.Prenda;
+import ar.edu.davinci.dvds20231cg9.domain.TipoPrenda;
+import ar.edu.davinci.dvds20231cg9.exceptions.BusinessException;
+public interface PrendaService {
+	
+	Prenda save(final Prenda prenda) throws BusinessException;
+	Prenda update(final Prenda prenda) throws BusinessException;
+	void delete(final Prenda prenda);
+	void delete(final Long id);
+	Prenda findById(final Long id) throws BusinessException;
+	
+	List<Prenda> list();
+	Page<Prenda> list(Pageable pageable);
+	
+	long count();
+	
+	List<TipoPrenda> getTipoPrendas();	
+}
