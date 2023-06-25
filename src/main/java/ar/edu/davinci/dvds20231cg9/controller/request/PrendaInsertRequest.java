b03fa9dd79c66a59b70eb20399ef6880ad88e4eb
Juan Manuel Saavedra <juaanms@gmail.com>
2023-06-25 14:02:33 -0300

    hasta index.html

---
+++ src/main/java/ar/edu/davinci/dvds20231cg9/controller/request/PrendaInsertRequest.java
@@ -0,0 +1,23 @@
+package ar.edu.davinci.dvds20231cg9.controller.request;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class PrendaInsertRequest {
+
+	private String descripcion;
+
+	private String tipo;
+
+	private BigDecimal precioBase;
+}
+
